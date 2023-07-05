9b355f3539576e0888998eccab5b79248dced90e
Shubham Kumar <insshubh22@gmail.com>
2023-07-05 12:15:40 +0530

    Add files via upload

---
+++ python/love_Calc.py
@@ -0,0 +1,7 @@
+# import random
+# name = input("Enter your Name\n")
+# name_2 = input("Enter Your Partner Name\n")
+#
+# rando = random.randint(0, 100)
+# print(name + " love " + name_2 + " " + str(rando) + " % ")
+
---
+++ python/practice.py
@@ -0,0 +1,73 @@
+"""
+def highest_even(li):
+    high = -1
+    for i in li:
+        if i % 2 == 0:
+            high = max(i,   high)
+    return high
+
+
+print(highest_even([1, 2, 5, 6, 18, 10]))
+"""
+
+# Printing star Pattern
+#           *
+#         * * *
+#       * * * * *
+
+"""
+inp = int(input("Enter the number"))
+for i in range(0, inp):
+    for j in range(0, inp - i - 1):
+        print(" ",end=' ')
+    for k in range(0, 2*i + 1):
+        print('*', end=' ')
+    print()
+"""
+
+# Sort an Array using any Sorting Algo
+# BubbleSort->Assembly Line big val last me
+"""
+def sort_bubble(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n - 1-i):
+            if arr[j+1] <= arr[j]:
+                temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+
+"""
+
+
+def print_arr(ar):
+    n = len(ar)
+    for i in range(0, n):
+        print(ar[i])
+
+
+arr = [11, 3, 40, 5, 5, 7, 7]
+
+# SelectionSort-> Find the minimumVal index then swap it
+
+"""
+def selection_sort(ar):
+    n = len(arr)
+    for i in range(0, n - 1):
+        index = i
+        for j in range(i, n):
+            if arr[j] < arr[index]:
+                index = j
+        temp = arr[index]
+        arr[index] = arr[i]
+        arr[i] = temp
+
+"""
+
+# selection_sort(arr)
+#print_arr(arr)
+
+
+
+
+
